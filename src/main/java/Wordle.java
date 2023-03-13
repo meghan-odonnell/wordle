8d50b61714b794f970da2bd08a272be1e54276d8
Meghan O'Donnell <odonnell.meghan@gmail.com>
2023-03-13 01:38:44 -0400

    ANSI color test

--- src/main/java/Wordle.java
+++ src/main/java/Wordle.java
@@ -18,25 +18,30 @@ public class Wordle {
 
       int guessCount = 0;
       boolean hasCorrectWord = false;
-
+      final String ANSI_GREEN = "\u001B[32m";
+        System.out.println(ANSI_GREEN);
+        final String ANSI_RESET = "\u001B[0m";
+        System.out.println(ANSI_RESET);
+        final String ANSI_YELLOW = "\u001B[33m";
+        System.out.println(ANSI_YELLOW);
       String[] letters = new String[] {String.valueOf(inputWord.split(""))};
         System.out.println(letters);
 
       if (inputWord.equals(todaysWord)){
           hasCorrectWord = true;
       }
-
-      while (!hasCorrectWord && guessCount <= 6){
-          int inputWordIndex = 0;
-          int todaysWordIndex = 0;
-            String inputLetter = inputWord.substring(0,1);
-          int wordLength = todaysWord.length();
-
-          for (inputWordIndex = 0; inputWordIndex < wordLength; inputWordIndex++) {
-            for (todaysWordIndex = 0; todaysWordIndex < wordLength; todaysWordIndex++) {
-                if ()
-            }
-          }
+//
+//      while (!hasCorrectWord && guessCount <= 6){
+//          int inputWordIndex = 0;
+//          int todaysWordIndex = 0;
+//            String inputLetter = inputWord.substring(0,1);
+//          int wordLength = todaysWord.length();
+//
+//          for (inputWordIndex = 0; inputWordIndex < wordLength; inputWordIndex++) {
+//            for (todaysWordIndex = 0; todaysWordIndex < wordLength; todaysWordIndex++) {
+//                if ()
+//            }
+//          }
 
 
 
