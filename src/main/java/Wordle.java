fdaf8e0080efb6a9ff904c8667a6aab18029a5e9
Meghan O'Donnell <odonnell.meghan@gmail.com>
2023-03-18 23:47:52 -0500

    update output formatting

--- src/main/java/Wordle.java
+++ src/main/java/Wordle.java
@@ -1,6 +1,6 @@
 
 
-import java.sql.SQLOutput;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -12,60 +12,63 @@ public class Wordle {
       Boolean running = false;
       Scanner input = new Scanner(System.in);
       List<String> words = new ArrayList<>();
+
+
       words.add("hello");
       words.add("world");
-
       String guess = "howdy";
       String todaysWord = words.get(new Random().nextInt(words.size()));
 
       int guessCount = 1;
       boolean hasCorrectWord = false;
       final String ANSI_GREEN = "\u001B[32m";
-        System.out.println(ANSI_GREEN);
-        final String ANSI_RESET = "\u001B[0m";
-        System.out.println(ANSI_RESET);
-        final String ANSI_YELLOW = "\u001B[33m";
-        System.out.println(ANSI_YELLOW);
-        running = true;
-        while(running) {
-          System.out.println("Enter a word to guess");
-          System.out.println("__ __ __ __ __ __");
-          guess = input.next();
+      final String ANSI_RESET = "\u001B[0m";
+      final String ANSI_YELLOW = "\u001B[33m";
+
+
+      running = true;
+
+      System.out.println("Enter a word to guess");
+      System.out.println("__  __  __  __  __");
+
+
+      while(running && guessCount<=6) {
+        guess = input.next();
           if (guess.equals(todaysWord)) {
             System.out.println("Woohoo! You got it correct in " + guessCount + " guesses!");
             hasCorrectWord = true;
             running = false;
           }
+          System.out.println("+--------------------+");
+          System.out.print("| ");
           StringBuilder b = new StringBuilder();
+
+
           for (int i = 0; i < guess.length(); i++) {
+            StringBuilder d = new StringBuilder();
             char c = guess.charAt(i);
             if (todaysWord.charAt(i) == c) {
+              System.out.print(d.append(ANSI_GREEN + c + ANSI_RESET));
+              System.out.print(" | ");
               b.append(ANSI_GREEN + c + ANSI_RESET);
             } else if (todaysWord.contains(Character.toString(c))) {
+              System.out.print(d.append(ANSI_YELLOW + c + ANSI_RESET));
+              System.out.print(" | ");
               b.append(ANSI_YELLOW + c + ANSI_RESET);
             } else {
+              System.out.print(d.append(c) + " | ");
               b.append(c);
 
             }
           }
-          System.out.println(b);
-          System.out.println("Count:" + guessCount);
+        System.out.println("");
+          System.out.println("+--------------------+");
+         // System.out.println("Count:" + guessCount);
           guessCount++;
-
         }
+
+
       System.out.println("Thanks for playing  :)");
-//
-//      while (!hasCorrectWord && guessCount <= 6){
-//          int inputWordIndex = 0;
-//          int todaysWordIndex = 0;
-//            String inputLetter = inputWord.substring(0,1);
-//          int wordLength = todaysWord.length();
-//
-//          for (inputWordIndex = 0; inputWordIndex < wordLength; inputWordIndex++) {
-//            for (todaysWordIndex = 0; todaysWordIndex < wordLength; todaysWordIndex++) {
-//                if ()
-//            }
-//          }
 
 
 
